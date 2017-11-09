247400eac2d2924d639c2afd4bfe84c97e097fbd
Heapah <33457316+Heapah@users.noreply.github.com>
2017-11-09 13:56:16 +0000

    Update Flightsystem.cpp

--- Flightsystem.cpp
+++ Flightsystem.cpp
@@ -1,93 +1,202 @@
 //Ethan Heap
+
 //01/11/17
+
 //Flight Control Assignment OOP
+
+
 
 #pragma once
 
+
+
 #include<iostream>
+
 #include<string>
+
 #include<vector>
+
 #include"Flightsystem.h"
 
+
+
 Flightsystem::~Flightsystem()
+
 {
+
 }
+
+
 
 //Getters and Setters for Flightsystem
 
+
+
 Flightsystem::Flightsystem(std::vector<Aircraft> aircraft, std::vector<Aircraft> aircraftList, std::string addAircraft, std::string removeAircraft, std::string listAllAircraft,
+
 	std::string listAllCruisingAircraft, std::string changeHeading, std::string getHeading, int changeAltitude, int getAltitude, int numAircraftInSector) :
+
 	addAircraft_{ addAircraft }, removeAircraft_{ removeAircraft }, listAllAircraft_{ listAllAircraft }, listAllCruisingAircraft_{ listAllCruisingAircraft },
+
 	changeHeading_{ changeHeading }, getHeading_{ getHeading }, changeAltitude_{ changeAltitude }, getAltitude_{ getAltitude }, numAircraftInSector_{ numAircraftInSector } {}
 
-    void Flightsystem::SetaddAircraft_(std::string addAircraft) {
-	    addAircraft_ = addAircraft;
+
+
+void Flightsystem::SetaddAircraft_(std::string addAircraft) {
+
+	addAircraft_ = addAircraft;
+
 }
-    std::string Flightsystem::GetaddAircraft_() const {
-	    return addAircraft_;
+
+std::string Flightsystem::GetaddAircraft_() const {
+
+	return addAircraft_;
+
 }
 
 
-	void Flightsystem::SetremoveAircraft_(std::string removeAircraft) {
-		removeAircraft_ = removeAircraft;
-	}
-	std::string Flightsystem::GetremoveAircraft_() const {
-		return removeAircraft_;
-	}
 
 
-	void Flightsystem::SetlistAllAircraft_(std::string listAllAircraft) {
-		listAllAircraft_ = listAllAircraft;
-	}
-	std::string Flightsystem::GetlistAllAircraft_() const {
-		return listAllAircraft_;
-	}
+
+void Flightsystem::SetremoveAircraft_(std::string removeAircraft) {
+
+	removeAircraft_ = removeAircraft;
+
+}
+
+std::string Flightsystem::GetremoveAircraft_() const {
+
+	return removeAircraft_;
+
+}
 
 
-	void Flightsystem::SetlistAllCruisingAircraft_(std::string listAllCruisingAircraft) {
-		listAllCruisingAircraft_ = listAllCruisingAircraft;
-	}
-	std::string Flightsystem::GetlistAllCruisingAircraft_() const {
-		return listAllCruisingAircraft_;
-	}
 
 
-	void Flightsystem::SetchangeHeading_(std::string changeHeading) {
-		changeHeading_ = changeHeading;
-	}
-	std::string Flightsystem::GetchangeHeading_() const {
-		return changeHeading_;
-	}
+
+void Flightsystem::SetlistAllAircraft_(std::string listAllAircraft) {
+
+	listAllAircraft_ = listAllAircraft;
+
+}
+
+std::string Flightsystem::GetlistAllAircraft_() const {
+
+	return listAllAircraft_;
+
+}
 
 
-	void Flightsystem::SetgetHeading_(std::string getHeading) {
-		getHeading_ = getHeading;
-	}
-	std::string Flightsystem::GetgetHeading_() const {
-		return getHeading_;
-	}
 
 
-	void Flightsystem::SetchangeAltitude_(int changeAltitude) {
-		changeAltitude_ = changeAltitude;
-	}
-	int Flightsystem::GetchangeAltitude_() const {
-		return changeAltitude_;
-	}
+
+void Flightsystem::SetlistAllCruisingAircraft_(std::string listAllCruisingAircraft) {
+
+	listAllCruisingAircraft_ = listAllCruisingAircraft;
+
+}
+
+std::string Flightsystem::GetlistAllCruisingAircraft_() const {
+
+	return listAllCruisingAircraft_;
+
+}
 
 
-	void Flightsystem::SetgetAltitude_(int getAltitude) {
-		getAltitude_ = getAltitude;
-	}
-	int Flightsystem::GetgetAltitude_() const {
-		return getAltitude_;
-	}
 
 
-	void Flightsystem::SetnumAircraftInSector_(int numAircraftInSector) {
-		numAircraftInSector_ = numAircraftInSector;
-	}
-	int Flightsystem::GetnumAircraftInSector_() const {
-		return numAircraftInSector_;
-	}
 
+void Flightsystem::SetchangeHeading_(std::string changeHeading) {
+
+	changeHeading_ = changeHeading;
+
+}
+
+std::string Flightsystem::GetchangeHeading_() const {
+
+	return changeHeading_;
+
+}
+
+
+
+
+
+void Flightsystem::SetgetHeading_(std::string getHeading) {
+
+	getHeading_ = getHeading;
+
+}
+
+std::string Flightsystem::GetgetHeading_() const {
+
+	return getHeading_;
+
+}
+
+
+
+
+
+void Flightsystem::SetchangeAltitude_(int changeAltitude) {
+
+	changeAltitude_ = changeAltitude;
+
+}
+
+int Flightsystem::GetchangeAltitude_() const {
+
+	return changeAltitude_;
+
+}
+
+
+
+
+
+void Flightsystem::SetgetAltitude_(int getAltitude) {
+
+	getAltitude_ = getAltitude;
+
+}
+
+int Flightsystem::GetgetAltitude_() const {
+
+	return getAltitude_;
+
+}
+
+
+
+
+
+void Flightsystem::SetnumAircraftInSector_(int numAircraftInSector) {
+
+	numAircraftInSector_ = numAircraftInSector;
+
+}
+
+int Flightsystem::GetnumAircraftInSector_() const {
+
+	return numAircraftInSector_;
+
+}
+
+// Adding an Aircraft
+
+void Flightsystem::addAircraft(std::string flightNumber, std::string airline, std::string airlineType, int altitude, int groundSpeed, std::string gridReference, int heading) {
+	Aircraft a(flightNumber, airline, airlineType, altitude, groundSpeed, gridReference, heading)
+}
+bool isFound = false
+for (int i = 0; i < aircraftList_.size(); i++) {
+	if (aircraftList_[i].GetFlightNumber() == flightNumber) {
+		isFound = true;
+	}
+}
+
+if (isFound = true) {
+	std::cout << "Flight " << flightNumber << " is already in the system" << std::endl;
+}
+else {
+	aircraftList_.push_back(Aircraft(a));
+}
